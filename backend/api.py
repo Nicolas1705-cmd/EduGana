7a0108d435e792c2c435cafd0b1e6fa8d2c368f7
01SULK1A1201I01\Admin <lcardoza@senati.pe>
2025-11-22 11:31:05 -0500

    no message

--- backend/api.py
+++ backend/api.py
@@ -11,6 +11,7 @@ import listar_recompensas
 import usuarios
 import cupones
 import matricula
+import profesores
 
 
 # RYDER
@@ -34,6 +35,54 @@ app.add_url_rule(
     view_func=colegio.registrar_colegio, # La función del módulo 'seccion'
     methods=['GET']   # Los métodos HTTP permitidos
 )
+
+
+# MOSNERRAT
+app.add_url_rule(
+    '/addProfesor',         # La URL a mapear
+    view_func=profesores.agregar_profesor, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+# JOSE check
+app.add_url_rule(
+    '/listRecompensas',         # La URL a mapear
+    view_func=listar_recompensas.listar_recompensas, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+
+# CUEVA 
+app.add_url_rule(
+    '/registrarAsistencia',         # La URL a mapear
+    view_func=registro_de_control_asistencia.registrar_asistencia, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+# ANDERSON 
+app.add_url_rule(
+    '/listAsistencia',         # La URL a mapear
+    view_func=Historial.obtener_asistencias, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+
+
+# RODRIGO 
+app.add_url_rule(
+    '/registrarUsuario',         # La URL a mapear
+    view_func=usuarios.registrar_usuario, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+
+
+
 
 
 
