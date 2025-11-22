196b38e228d3635d0c5106c10fab06068c7d9870
01SULK1A1201I01\Admin <lcardoza@senati.pe>
2025-11-22 11:11:53 -0500

    no message

--- backend/api.py
+++ backend/api.py
@@ -5,16 +5,35 @@ import curso
 import colegio
 import historialdecanjes
 import Historial
-import tienda
+# import tienda
 import registro_de_control_asistencia
 import listar_recompensas
 import usuarios
 import cupones
+import matricula
 
 
-# 1. Importa y registra directamente la función:
+# RYDER
 app.add_url_rule(
-    '/bienvenido',         # La URL a mapear
-    view_func=seccion.bienvenido, # La función del módulo 'seccion'
+    '/addCurso',         # La URL a mapear
+    view_func=curso.registrar_inscripcion, # La función del módulo 'seccion'
     methods=['GET']   # Los métodos HTTP permitidos
 )
+
+# SEBASTIAN check
+app.add_url_rule(
+    '/addMatricula',         # La URL a mapear
+    view_func=matricula.registrar_matricula, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+# BRAYAN
+app.add_url_rule(
+    '/addColegio',         # La URL a mapear
+    view_func=colegio.registrar_colegio, # La función del módulo 'seccion'
+    methods=['GET']   # Los métodos HTTP permitidos
+)
+
+
+
